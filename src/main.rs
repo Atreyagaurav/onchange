8f3595a6817e0efd812b7f31f4ca6257dfde32c3
Gaurav <allmanpride@gmail.com>
2023-02-14 15:28:02 -0500

    Add config file support for default actions

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use clap::Parser;
+use colored::Colorize;
+use config;
 use new_string_template::template::Template;
-use notify_debouncer_mini::{new_debouncer, notify::*, DebouncedEventKind};
+use notify_debouncer_mini::{new_debouncer, notify, DebouncedEventKind};
 use std::time::Duration;
 use std::{
     collections::HashMap,
@@ -12,6 +14,16 @@ use subprocess::Exec;
 
 #[derive(Parser)]
 struct Cli {
+    /// Config file
+    ///
+    /// If none given it'll search from the following paths in order:
+    ///
+    /// - "/etc/onchange.toml"
+    /// - "~/.config/onchange.toml"
+    /// - ".onchange.toml"
+    /// The later will overwrite the former if same config is present.
+    #[arg(short, long)]
+    config: Option<String>,
     /// Scan duration in miliseconds
     #[arg(short, long, default_value = "500")]
     duration: u64,
@@ -22,7 +34,7 @@ struct Cli {
     #[arg(short, long, action)]
     r#async: bool,
     /// List paths to watch
-    #[arg(short, long, default_value = "Change Detected: {path}")]
+    #[arg(short, long, default_value = "{path}")]
     template: String,
     /// List paths to watch
     #[arg(num_args(1..), required(true))]
@@ -36,45 +48,127 @@ fn template_vars(path: &PathBuf, pwd: &PathBuf) -> HashMap<&'static str, String>
     let mut map: HashMap<&str, String> = HashMap::new();
     map.insert(
         "name",
-        format!("{:?}", path.file_stem().unwrap_or_default()),
+        path.file_stem()
+            .unwrap_or_default()
+            .to_string_lossy()
+            .to_string(),
     );
-    map.insert("ext", format!("{:?}", path.extension().unwrap_or_default()));
+    map.insert(
+        "ext",
+        path.extension()
+            .unwrap_or_default()
+            .to_string_lossy()
+            .to_string(),
+    );
     map.insert(
         "name.ext",
-        format!("{:?}", path.file_name().unwrap_or_default()),
+        path.file_name()
+            .unwrap_or_default()
+            .to_string_lossy()
+            .to_string(),
     );
-    map.insert("pwd", format!("{:?}", pwd));
-    map.insert("path", format!("{:?}", path));
+    map.insert("pwd", pwd.to_string_lossy().to_string());
+    map.insert("path", path.to_string_lossy().to_string());
     map.insert(
         "rpath",
-        format!("{:?}", pathdiff::diff_paths(path, pwd).unwrap()),
+        pathdiff::diff_paths(path, pwd)
+            .unwrap()
+            .to_string_lossy()
+            .to_string(),
     );
     let parent = path.parent().unwrap_or(Path::new("/"));
-    map.insert("dir", format!("{:?}", parent));
+    map.insert("dir", parent.to_string_lossy().to_string());
     map.insert(
         "rdir",
-        format!("{:?}", pathdiff::diff_paths(parent, pwd).unwrap()),
+        pathdiff::diff_paths(parent, pwd)
+            .unwrap()
+            .to_string_lossy()
+            .to_string(),
     );
     map
 }
 
+fn get_config(conf: &Option<String>) -> Result<config::Config, String> {
+    if let Some(conf_file) = conf {
+        return config::Config::builder()
+            .add_source(config::File::with_name(conf_file))
+            .build()
+            .map_err(|e| e.to_string());
+    }
+    config::Config::builder()
+        .add_source(
+            vec![
+                PathBuf::from("/etc/onchange.toml"),
+                PathBuf::from(format!(
+                    "{}/.config/onchange.toml",
+                    std::env::var("HOME").unwrap_or_default()
+                )),
+                PathBuf::from(".onchange.toml"),
+            ]
+            .iter()
+            .filter(|f| f.exists())
+            .map(|f| config::File::from(f.as_path()))
+            .collect::<Vec<config::File<_, _>>>(),
+        )
+        .build()
+        .map_err(|e| e.to_string())
+}
+
+fn ext_map_from_config<'a>(
+    conf: &'a HashMap<String, HashMap<String, String>>,
+) -> HashMap<String, Template> {
+    let mut extmap = HashMap::new();
+    for (k, v) in conf {
+        println!("{}: {} ⇒ {}", "Rule".blue().bold(), k, v["command"]);
+        for ext in v["extensions"].split(" ") {
+            extmap.insert(ext.to_string(), Template::new(&v["command"]));
+        }
+    }
+    extmap
+}
+
+fn render_command(
+    cmd: &Option<Template>,
+    conf_map: &HashMap<String, Template>,
+    map: HashMap<&str, String>,
+) -> String {
+    if let Some(templ) = cmd {
+        return templ.render_nofail(&map);
+    }
+    if let Some(templ) = conf_map.get(&(map["ext"].clone())) {
+        return templ.render_nofail(&map);
+    }
+    return String::from("");
+}
+
 fn main() {
     let args = Cli::parse();
+    let conf: HashMap<String, HashMap<String, String>> =
+        get_config(&args.config).unwrap().try_deserialize().unwrap();
+    let conf_map = ext_map_from_config(&conf);
     let cwd = env::current_dir().unwrap();
-    let cng_templ = Template::new(args.template);
-    let cmd_templ = Template::new(args.command.join(" "));
+    let cng_templ = if args.template.len() > 0 {
+        Some(Template::new(args.template))
+    } else {
+        None
+    };
+    let cmd_templ = if args.command.len() > 0 {
+        Some(Template::new(args.command.join(" ")))
+    } else {
+        None
+    };
     let (tx, rx) = std::sync::mpsc::channel();
 
     let mut debouncer = new_debouncer(Duration::from_millis(args.duration), None, tx).unwrap();
 
     let rm = if args.recursive {
-        RecursiveMode::Recursive
+        notify::RecursiveMode::Recursive
     } else {
-        RecursiveMode::NonRecursive
+        notify::RecursiveMode::NonRecursive
     };
     let watcher = debouncer.watcher();
     for path in args.watch {
-        println!("watching: {:?}", path);
+        println!("{}: {:?}", "Watch".bold().yellow(), path);
         watcher.watch(path.as_ref(), rm).unwrap();
     }
 
@@ -84,8 +178,15 @@ fn main() {
                 DebouncedEventKind::Any => {
                     let mut map = template_vars(&event.path, &cwd);
                     map.insert("event", format!("{:?}", event));
-                    println!("{}", cng_templ.render_nofail(&map));
-                    let cmd = cmd_templ.render_nofail(&map);
+                    if let Some(templ) = &cng_templ {
+                        println!(
+                            "{}: {}",
+                            "Changed".bold().green(),
+                            templ.render_nofail(&map)
+                        );
+                    }
+                    let cmd = render_command(&cmd_templ, &conf_map, map);
+                    println!("{}: {}", "Run".bold().red(), cmd);
                     if args.r#async {
                         thread::spawn(|| {
                             Exec::shell(cmd).join().unwrap();
